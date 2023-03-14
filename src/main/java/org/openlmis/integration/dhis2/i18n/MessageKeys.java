cdb7a454dab4b389beb7a63a347cbd8742502190
anawrotsoldevelo <anawrot@soldevelo.com>
2023-03-14 16:01:13 +0100

    SELV3-577: Added DHIS2_ADMIN right

--- src/main/java/org/openlmis/integration/dhis2/i18n/MessageKeys.java
+++ src/main/java/org/openlmis/integration/dhis2/i18n/MessageKeys.java
@@ -96,7 +96,7 @@ public abstract class MessageKeys {
   public static final String ERROR_PERMISSION_CHECK_FAILED = ERROR_PREFIX
           + ".authorization.failed";
 
-  public static final String ERROR_USER_NOT_FOUND = ERROR_PREFIX + ".user.notFound";
+  public static final String ERROR_USER_NOT_FOUND = ERROR_PREFIX + ".authentication.user.notFound";
 
   private MessageKeys() {
     throw new UnsupportedOperationException();
